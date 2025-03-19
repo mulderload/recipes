a30704c1d65ca165f6f5929d0f2bc9c9a8b706e6
mulderload <200639147+mulderload@users.noreply.github.com>
2025-03-20 00:54:46 +0100

    CodeSigning - use new certificate 'Mulderload'

--- build/functions/CodeSigning.ps1
+++ build/functions/CodeSigning.ps1
@@ -1,6 +1,6 @@
 function CodeSigning.InstallCertificate {
     [Byte[]] $bytes = [System.Convert]::FromBase64String($Env:CERTIFICATE_BASE64)
-    [IO.File]::WriteAllBytes("steampatches.pfx", $bytes)
+    [IO.File]::WriteAllBytes("mulderload.pfx", $bytes)
 }
 
 function CodeSigning.SignExecutable {
@@ -8,7 +8,7 @@ function CodeSigning.SignExecutable {
         [string]$exePath
     )
     # /td sha256 is not working with this cert
-    $process = Start-Process "C:/Program Files (x86)/Microsoft SDKs/ClickOnce/SignTool/signtool.exe" "sign /f steampatches.pfx /p $Env:CERTIFICATE_PASSWORD /t http://timestamp.digicert.com /fd sha256 $exePath" -PassThru -Wait
+    $process = Start-Process "C:/Program Files (x86)/Microsoft SDKs/ClickOnce/SignTool/signtool.exe" "sign /f mulderload.pfx /p $Env:CERTIFICATE_PASSWORD /t http://timestamp.digicert.com /fd sha256 $exePath" -PassThru -Wait
 
     if ($process.exitCode -ne 0) {
         throw "Error with signtool $exePath"
